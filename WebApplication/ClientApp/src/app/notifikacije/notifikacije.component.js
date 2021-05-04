443e9737d4978142592349ef06ec40f96ec531f6
Dragana Trivunovic <dragana.trivunovic@vega.local@git config --global user.email dragana.trivunovic@vega.local>
2021-05-04 12:52:52 +0200

    Finished notifications front end

--- WebApplication/ClientApp/src/app/notifikacije/notifikacije.component.js
+++ WebApplication/ClientApp/src/app/notifikacije/notifikacije.component.js
@@ -10,7 +10,73 @@ exports.NotifikacijeComponent = void 0;
 var core_1 = require("@angular/core");
 var NotifikacijeComponent = /** @class */ (function () {
     function NotifikacijeComponent() {
+        var _this = this;
+        //TODO get all notifications and filter into unreadNotifications
+        this.unreadNotifications = new Array();
+        this.tempNotifications = new Array();
+        this.unreadNotifications.push({ id: '1', text: 'testtttttt', color: 'red', icon: 'error', timestamp: '21.10.1998.', tied: false, tiedTo: '' });
+        this.unreadNotifications.push({ id: '2', text: 'testtttttt', color: 'red', icon: 'error', timestamp: '21.10.1998.', tied: false, tiedTo: '' });
+        this.unreadNotifications.push({ id: '3', text: 'testtttttt', color: 'blue', icon: 'notification_important', timestamp: '21.10.1998.', tied: true, tiedTo: 'smth' });
+        this.unreadNotifications.push({ id: '4', text: 'testtttttt', color: 'blue', icon: 'notification_important', timestamp: '21.10.1998.', tied: true, tiedTo: 'smth' });
+        this.unreadNotifications.push({ id: '5', text: 'testtttttt', color: 'yellow', icon: 'warning', timestamp: '21.10.1998.', tied: true, tiedTo: 'smth' });
+        this.unreadNotifications.push({ id: '6', text: 'testtttttt', color: 'yellow', icon: 'warning', timestamp: '21.10.1998.', tied: true, tiedTo: 'smth' });
+        this.unreadNotifications.push({ id: '7', text: 'testtttttt', color: 'green', icon: 'done_all', timestamp: '21.10.1998.', tied: true, tiedTo: 'smth' });
+        this.unreadNotifications.push({ id: '8', text: 'testtttttt', color: 'green', icon: 'done_all', timestamp: '21.10.1998.', tied: true, tiedTo: 'smth' });
+        this.unreadNotifications.push({ id: '9', text: 'testtttttt', color: 'green', icon: 'done_all', timestamp: '21.10.1998.', tied: true, tiedTo: 'smth' });
+        this.unreadNotifications.forEach(function (val) { return _this.tempNotifications.push(Object.assign({}, val)); });
     }
+    NotifikacijeComponent.prototype.markAllRead = function () {
+        this.unreadNotifications = new Array();
+        this.tempNotifications = new Array();
+        //TODO update db
+    };
+    NotifikacijeComponent.prototype.markRead = function (id) {
+        for (var i = 0; i < this.unreadNotifications.length; i++) {
+            if (this.unreadNotifications[i].id === id) {
+                this.unreadNotifications.splice(i, 1);
+            }
+        }
+        for (var i = 0; i < this.tempNotifications.length; i++) {
+            if (this.tempNotifications[i].id === id) {
+                this.tempNotifications.splice(i, 1);
+            }
+        }
+        //TODO update db
+    };
+    NotifikacijeComponent.prototype.routeTo = function (tiedTo) {
+        //TODO 
+    };
+    NotifikacijeComponent.prototype.showAll = function () {
+    };
+    NotifikacijeComponent.prototype.showUnread = function () {
+        var _this = this;
+        this.unreadNotifications = new Array();
+        this.tempNotifications.forEach(function (val) { return _this.unreadNotifications.push(Object.assign({}, val)); });
+    };
+    NotifikacijeComponent.prototype.showError = function () {
+        var _this = this;
+        this.unreadNotifications = new Array();
+        var result = this.tempNotifications.filter(function (o) { return o.icon === 'error'; });
+        result.map(function (val) { return _this.unreadNotifications.push(Object.assign({}, val)); });
+    };
+    NotifikacijeComponent.prototype.showInfo = function () {
+        var _this = this;
+        this.unreadNotifications = new Array();
+        var result = this.tempNotifications.filter(function (o) { return o.icon === 'notification_important'; });
+        result.map(function (val) { return _this.unreadNotifications.push(Object.assign({}, val)); });
+    };
+    NotifikacijeComponent.prototype.showSuccess = function () {
+        var _this = this;
+        this.unreadNotifications = new Array();
+        var result = this.tempNotifications.filter(function (o) { return o.icon === 'done_all'; });
+        result.map(function (val) { return _this.unreadNotifications.push(Object.assign({}, val)); });
+    };
+    NotifikacijeComponent.prototype.showWarning = function () {
+        var _this = this;
+        this.unreadNotifications = new Array();
+        var result = this.tempNotifications.filter(function (o) { return o.icon === 'warning'; });
+        result.map(function (val) { return _this.unreadNotifications.push(Object.assign({}, val)); });
+    };
     NotifikacijeComponent = __decorate([
         core_1.Component({
             selector: 'notifikacije',
--- WebApplication/ClientApp/src/app/notifikacije/notifikacije.component.ts
+++ WebApplication/ClientApp/src/app/notifikacije/notifikacije.component.ts
@@ -12,5 +12,83 @@ import { MatSort } from '@angular/material/sort';
 })
 
 export class NotifikacijeComponent {
+  unreadNotifications: Array<{ id:string,text: string, color: string, icon: string, timestamp: string, tied: boolean, tiedTo: string }>;
+  tempNotifications: Array<{ id:string,text: string, color: string, icon: string, timestamp: string, tied: boolean, tiedTo: string }>;
 
+  constructor() {
+    //TODO get all notifications and filter into unreadNotifications
+    this.unreadNotifications = new Array();
+    this.tempNotifications = new Array();
+    this.unreadNotifications.push({ id:'1',text: 'testtttttt', color: 'red', icon: 'error', timestamp: '21.10.1998.', tied:false,tiedTo:'' })
+    this.unreadNotifications.push({ id:'2',text: 'testtttttt', color: 'red', icon: 'error', timestamp: '21.10.1998.', tied:false,tiedTo:'' })
+    this.unreadNotifications.push({ id: '3',text: 'testtttttt', color: 'blue', icon: 'notification_important', timestamp: '21.10.1998.', tied:true,tiedTo:'smth' })
+    this.unreadNotifications.push({ id: '4',text: 'testtttttt', color: 'blue', icon: 'notification_important', timestamp: '21.10.1998.', tied:true,tiedTo:'smth' })
+    this.unreadNotifications.push({ id: '5',text: 'testtttttt', color: 'yellow', icon: 'warning', timestamp: '21.10.1998.', tied:true,tiedTo:'smth' })
+    this.unreadNotifications.push({ id: '6',text: 'testtttttt', color: 'yellow', icon: 'warning', timestamp: '21.10.1998.', tied:true,tiedTo:'smth' })
+    this.unreadNotifications.push({ id: '7', text: 'testtttttt', color: 'green', icon: 'done_all', timestamp: '21.10.1998.', tied: true, tiedTo: 'smth' })
+    this.unreadNotifications.push({ id: '8', text: 'testtttttt', color: 'green', icon: 'done_all', timestamp: '21.10.1998.', tied: true, tiedTo: 'smth' })
+    this.unreadNotifications.push({ id: '9', text: 'testtttttt', color: 'green', icon: 'done_all', timestamp: '21.10.1998.', tied: true, tiedTo: 'smth' })
+    this.unreadNotifications.forEach(val => this.tempNotifications.push((<any>Object).assign({}, val)));
+  }
+
+  markAllRead(): void {
+    this.unreadNotifications = new Array();
+    this.tempNotifications = new Array();
+    //TODO update db
+  }
+
+  markRead(id): void {
+    for (var i = 0; i < this.unreadNotifications.length; i++) {
+
+      if (this.unreadNotifications[i].id === id) {
+
+        this.unreadNotifications.splice(i, 1);
+      }
+    }
+    for (var i = 0; i < this.tempNotifications.length; i++) {
+
+      if (this.tempNotifications[i].id === id) {
+
+        this.tempNotifications.splice(i, 1);
+      }
+    }
+    //TODO update db
+  }
+
+  routeTo(tiedTo): void {
+    //TODO 
+  }
+
+  showAll(): void {
+
+  }
+
+  showUnread(): void {
+    this.unreadNotifications = new Array();
+    this.tempNotifications.forEach(val => this.unreadNotifications.push((<any>Object).assign({}, val)));
+  }
+
+  showError(): void {
+    this.unreadNotifications = new Array();
+    let result = this.tempNotifications.filter(o => o.icon === 'error');
+    result.map(val => this.unreadNotifications.push((<any>Object).assign({}, val)));
+  }
+
+  showInfo(): void {
+    this.unreadNotifications = new Array();
+    let result = this.tempNotifications.filter(o => o.icon === 'notification_important');
+    result.map(val => this.unreadNotifications.push((<any>Object).assign({}, val)));
+  }
+
+  showSuccess(): void {
+    this.unreadNotifications = new Array();
+    let result = this.tempNotifications.filter(o => o.icon === 'done_all');
+    result.map(val => this.unreadNotifications.push((<any>Object).assign({}, val)));
+  }
+
+  showWarning(): void {
+    this.unreadNotifications = new Array();
+    let result = this.tempNotifications.filter(o => o.icon === 'warning');
+    result.map(val => this.unreadNotifications.push((<any>Object).assign({}, val)));
+  }
 }
